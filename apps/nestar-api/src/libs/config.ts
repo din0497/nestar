0e3f757040a0c954384032c2aa547e59e7f87ebb
Din <rivojidin97@gmail.com>
2024-08-30 11:39:02 +0900

    feat: develop single and multi image uploader

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,4 +1,6 @@
 import { ObjectId } from 'bson';
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
 
 export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews', 'memberRank'];
 export const availableMembersSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews'];
@@ -14,6 +16,17 @@ export const availablePropertySorts = [
 ];
 
 export const availableBoardArticles = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
+
+// IMAGE CONFIGURATION (config.js)
+
+
+
+export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const getSerialForImage = (filename: string) => {
+	const ext = path.parse(filename).ext;
+	return uuidv4() + ext;
+};
+
 
 export const shapeIntoMongoObjId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
