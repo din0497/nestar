9ad6f6c9de71e74ed8de32c76ebe97805fef9414
Din <rivojidin97@gmail.com>
2024-09-01 00:53:54 +0900

    feat: ZQ-TASK solved

--- train.ts
+++ train.ts
@@ -1,12 +1,8 @@
 /* 
-ZP-TASK:
+ZQ-TASK:
 
-shunday function yozing, u 2 ta array parametr qabul qilsin. Siz bu ikki arrayning qiymatlari o'xshash bo'lishini (ya'ni, ularning barcha elementlari bir xil bo'lishini) tekshirishingiz kerak.
-
-MASALAN: 
-areArraysEqual([1, 2, 3], [3, 1, 2]) // true
-areArraysEqual([1, 2, 3], [3, 1, 2, 1]) // true
-areArraysEqual([1, 2, 3], [4, 1, 2]) // false
+Shunday function yozing, u parametridagi array ichida 2 marta qaytarilgan sonlarni alohida araryda qaytarsin.
+MASALAN: findDuplicates([1,2,3,4,5,4,3,4]) return [3, 4]
 
 
 */
@@ -15,16 +11,14 @@ import { log } from 'console';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
-const areArraysEqual = (arr1: number[], arr2: number[]): boolean => {
-
-	for (const num of new Set(arr1)) {
-		if (!new Set(arr2).has(num)) {
-			return false
+const findDuplicates = (arr: number[]) => {
+	const result = []
+	for (const num of arr) {
+		if (arr.filter(ele => ele === num).length > 1 && !result.includes(num)) {
+			result.push(num)
 		}
 	}
-	return true
+	return result
 }
 
-log(areArraysEqual([1, 2, 3], [3, 1, 2]));
-log(areArraysEqual([1, 2, 3], [3, 1, 2, 1]));
-log(areArraysEqual([1, 2, 3], [4, 1, 2]));
+log(findDuplicates([1, 2, 3, 4, 5, 4, 3, 4]))
