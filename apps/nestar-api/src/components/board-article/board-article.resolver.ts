eb527cb2df056573f5ce400acfec3f02578e6a36
Din <rivojidin97@gmail.com>
2024-09-06 17:16:00 +0900

    feat: develop boardArticle related graphql apis

--- apps/nestar-api/src/components/board-article/board-article.resolver.ts
+++ apps/nestar-api/src/components/board-article/board-article.resolver.ts
@@ -21,5 +21,50 @@ import { RolesGuard } from '../auth/guards/roles.guard';
 export class BoardArticleResolver {
     constructor(private readonly boardArticleService: BoardArticleService) { }
 
-   
+    @UseGuards(AuthGuard)
+    @Mutation((returns) => BoardArticle)
+    public async createBoardArticle(
+        @Args('input') input: BoardArticleInput,
+        @AuthMember('_id') memberId: ObjectId,
+    ): Promise<BoardArticle> {
+        console.log('Mutation: CerateBoardAtricle');
+        return await this.boardArticleService.createBoardArticle(memberId, input);
+    }
+
+    @UseGuards(WithoutGuard)
+    @Query((returns) => BoardArticle)
+    public async getBoardArticle(
+        @Args('articleId') input: string,
+        @AuthMember('_id') memberId: ObjectId,
+    ): Promise<BoardArticle> {
+        console.log('Mutation: getBoardArticle');
+        const articleId = shapeIntoMongoObjId(input);
+        return await this.boardArticleService.getBoardArticle(memberId, articleId);
+    }
+
+    @UseGuards(AuthGuard)
+    @Mutation((returns) => BoardArticle)
+    public async updateBoardArticle(
+        @Args('input') input: BoardArticleUpdate,
+        @AuthMember('_id') memberId: ObjectId,
+    ): Promise<BoardArticle> {
+        console.log('Mutation: updateBoardArticle');
+        input._id = shapeIntoMongoObjId(input._id);
+        console.log({"input": input, 'memberId':memberId});
+        
+        return await this.boardArticleService.updateBoardArticle(memberId, input);
+    }
+    @UseGuards(WithoutGuard)
+    @Query((returns) => BoardArticles)
+    public async getBoardArticles(
+        @Args('input') input: BoardArticlesInquiry,
+        @AuthMember('_id') memberId: ObjectId,
+    ): Promise<BoardArticles> {
+        console.log('Mutation: getBoardArticles');
+        return await this.boardArticleService.getBoardArticles(memberId, input);
+    }
+
+
+    // admin
+
 }
