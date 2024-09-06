fca4cf67065775b8e712fe03de8e703a17bc8b19
Din <rivojidin97@gmail.com>
2024-09-06 20:19:32 +0900

    feat: ZR- TASK solved

--- train.ts
+++ train.ts
@@ -1,9 +1,8 @@
 /* 
-ZQ-TASK:
+ZR-TASK:
 
-Shunday function yozing, u parametridagi array ichida 2 marta qaytarilgan sonlarni alohida araryda qaytarsin.
-MASALAN: findDuplicates([1,2,3,4,5,4,3,4]) return [3, 4]
-
+Shunday function yozing, u parametridagi string ichidagi raqam va sonlarni sonini sanasin.
+MASALAN: countNumberAndLetters(“string152%\¥”) return {number:3, letter:6}
 
 */
 
@@ -11,14 +10,16 @@ import { log } from 'console';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
-const findDuplicates = (arr: number[]) => {
-	const result = []
-	for (const num of arr) {
-		if (arr.filter(ele => ele === num).length > 1 && !result.includes(num)) {
-			result.push(num)
+const countNumberAndLetters = (str: string): any => {
+	const result = { number: 0, letter: 0 }
+	for (const char of str) {
+		if (!isNaN(parseFloat(char)) && !/^[!@#$¥%^&*()_+\-=\[\]{};':"\\|,.<>\/?`~]+$/.test(char)) {
+			result.number++
+		} else if (isNaN(parseFloat(char)) && !/^[!@#¥%^&*()_+\-=\[\]{};':"\\|,.<>\/?`~]+$/.test(char)) {
+			result.letter++
 		}
 	}
 	return result
 }
 
-log(findDuplicates([1, 2, 3, 4, 5, 4, 3, 4]))
+console.log(countNumberAndLetters("string152%\¥"));
